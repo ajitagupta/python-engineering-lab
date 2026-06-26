b518b9648a5fad63af86e3565db7dcff60b46b85
Ajita Gupta <a.gupta@cross-ing.com>
2026-06-26 09:43:26 +0200

    Concept 1: REST API with GET and POST, clean numeric data model

--- 01-rest-api/app.py
+++ 01-rest-api/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-workouts = ["run", "swim", "badminton", "strength", "run", "rest", "swim"]
+workouts = [{"sport": "run", "distance_km": 6.0}, {"sport": "swim", "distance_km": 0.2}, {"sport": "badminton", "duration_min": 90}, {"sport": "strength", "duration_min": 35}, {"sport": "run", "distance_km": 8.0}, {"sport": "rest"}, {"sport": "swim", "distance_km": 0.400}]
 
 @app.get('/workouts')
 def get_workouts():
